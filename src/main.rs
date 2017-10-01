35aa8660219b4bf1367ea8d285a30d11773b8538
Denis Kolodin <denis.kolodin@xdev.re>
2017-10-01 13:31:44 +0300

    Add address checking

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate clap;
 extern crate web3;
 
+use clap::{Arg, App};
 use web3::futures::Future;
+use web3::types::Address;
 
 fn main() {
-  let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
-  let web3 = web3::Web3::new(http);
-  let accounts = web3.eth().accounts().wait().unwrap();
+    pretty_env_logger::init().unwrap();
+    let matches = App::new("Task 1")
+                      .version(crate_version!())
+                      .author("Denis Kolodin <deniskolodin@gmail.com")
+                      .about("Taks 1 example")
+                      .arg(Arg::with_name("ADDRESS")
+                               .help("Address to monitor")
+                               .required(true)
+                               .index(1))
+                      .get_matches();
 
-  println!("Accounts: {:?}", accounts);
+    let address = matches.value_of("ADDRESS").unwrap();
+    let address: Address  = address.parse().unwrap();
+
+    let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(http);
+
+    let balance = web3.eth().balance(address, None).wait().unwrap();
+
+    debug!("Balance is: {:?}", balance);
 }
