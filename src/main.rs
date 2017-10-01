c9da11bfad8678cbb73386f52dc1a607350ff79d
Denis Kolodin <denis.kolodin@xdev.re>
2017-10-01 14:04:01 +0300

    Print changes

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate clap;
 extern crate web3;
+extern crate futures;
+extern crate tokio_core;
 
 use clap::{Arg, App};
 use web3::futures::Future;
-use web3::types::Address;
+use web3::types::{Address, U256};
+use futures::Stream;
+use tokio_core::reactor::{Core, Interval};
+use std::time::Duration;
 
 fn main() {
     pretty_env_logger::init().unwrap();
@@ -27,7 +32,22 @@ fn main() {
     let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
     let web3 = web3::Web3::new(http);
 
-    let balance = web3.eth().balance(address, None).wait().unwrap();
 
-    debug!("Balance is: {:?}", balance);
+    let duration = Duration::new(5, 0);
+    let mut core = Core::new().unwrap();
+    let interval = Interval::new(duration, &core.handle()).unwrap();
+    let interval = interval.then(|_| {
+        web3.eth().balance(address, None)
+    });
+    let mut prev = U256::from(0);
+    let collector = interval.for_each(|new| {
+        if prev != new {
+            prev = new;
+            debug!("Balance is: {:?}", new);
+        }
+        Ok(())
+    });
+
+    core.run(collector).unwrap();
+
 }
