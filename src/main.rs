4f1088de3ed24d2e955103e73855319bcdaca336
Denis Kolodin <denis.kolodin@xdev.re>
2017-10-01 14:40:08 +0300

    Read addresses from file

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ extern crate clap;
 extern crate web3;
 extern crate futures;
 extern crate tokio_core;
+extern crate serde;
+extern crate serde_json;
 
 use clap::{Arg, App};
-use web3::futures::Future;
 use web3::types::{Address, U256};
-use futures::Stream;
+use futures::{future, Stream};
 use tokio_core::reactor::{Core, Interval};
 use std::time::Duration;
+use std::io::Read;
+use std::fs::File;
 
 fn main() {
     pretty_env_logger::init().unwrap();
@@ -20,34 +23,43 @@ fn main() {
                       .version(crate_version!())
                       .author("Denis Kolodin <deniskolodin@gmail.com")
                       .about("Taks 1 example")
-                      .arg(Arg::with_name("ADDRESS")
-                               .help("Address to monitor")
+                      .arg(Arg::with_name("FILE")
+                               .help("File with addresses")
                                .required(true)
                                .index(1))
                       .get_matches();
 
-    let address = matches.value_of("ADDRESS").unwrap();
-    let address: Address  = address.parse().unwrap();
+    let file = matches.value_of("FILE").unwrap();
+    let mut file = File::open(file).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
 
-    let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
-    let web3 = web3::Web3::new(http);
+    let addresses: Vec<Address> = serde_json::from_str(&contents).unwrap();
 
+    println!("{}", contents);
+
+    let mut core = Core::new().unwrap();
 
     let duration = Duration::new(5, 0);
-    let mut core = Core::new().unwrap();
-    let interval = Interval::new(duration, &core.handle()).unwrap();
-    let interval = interval.then(|_| {
-        web3.eth().balance(address, None)
-    });
-    let mut prev = U256::from(0);
-    let collector = interval.for_each(|new| {
-        if prev != new {
-            prev = new;
-            debug!("Balance is: {:?}", new);
-        }
-        Ok(())
+    let handle = core.handle();
+    let workers = addresses.into_iter().map(|address| {
+        let http = web3::transports::Http::with_event_loop("http://localhost:8545", &handle, 1).unwrap();
+        let web3 = web3::Web3::new(http);
+
+        let mut prev = U256::from(0);
+        Interval::new(duration, &handle).unwrap()
+        .then(move |_| {
+            web3.eth().balance(address, None)
+        })
+        .for_each(move |new| {
+            if prev != new {
+                prev = new;
+                debug!("Balance is: {:?}", new);
+            }
+            Ok(())
+        })
     });
 
+    let collector = future::join_all(workers);
     core.run(collector).unwrap();
-
 }
